e641549b2ac279c1d2636dac77c93f40297bdf22
son438520-droid <son438520@gmail.com>
2025-08-28 22:00:06 +0100

    Update client.js

--- src/components/lib/client.js
+++ src/components/lib/client.js
@@ -1,7 +1,7 @@
 import SanityClient from "@sanity/client";
 
 export const client = SanityClient({
-  projectId: "oLS90aBnu",
+  projectId: "7y2hiuox",
   dataset: "production",
   apiVersion: "2024-09-26",
   useCdn: true,
